83129840294c4847e55d4de46f58940a5483adb2
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-16 13:17:23 +0100

    Add initial route handlers and test endpoints via postman

--- services/cars/routes.js
+++ services/cars/routes.js
@@ -11,4 +11,36 @@ router.get('/cars', async (req, res, next) => {
   }
 })
 
+router.get('/cars/:id', (req, res, next) => {
+  try {
+    res.json({message: "Get Car by ID"})
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.post('/cars', (req, res, next) => {
+  try {
+    res.json({message: "Post New Car"})
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.put('/cars/:id', (req, res, next) => {
+  try {
+    res.json({message: "Update Car by ID"})
+  } catch (err) {
+    next(err);
+  }
+})
+
+router.delete('/cars/:id', (req, res, next) => {
+  try {
+    res.json({message: "Delete Car by ID"})
+  } catch (err) {
+    next(err);
+  }
+})
+
 module.exports = router;
